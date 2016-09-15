ff94e3e226ad2c0fd7cae88e970c1961e2a72e75
xingychb <xingychb@yonyou.com>
2016-09-15 22:41:46 +0800

    init

--- js/index.js
+++ js/index.js
@@ -13,23 +13,18 @@ jQuery(".over").slide({
 });
 $(document).scroll(function(){
     var h = $(document).scrollTop() + $(window).height();
-
     var h2 = $(".footer").offset().top - 10;
     if ( h >=  h2 ){
         $(".cover").css({
             position: "absolute",
             bottom : "10px"
         })
-        console.log(123)
     }else{
         $(".cover").css({
             position: "fixed",
             bottom : "0"
         })
     }
-    //console.log(h + "......."+ h2);
-
-   // console.log(h)
 });
 function closeicon(){
     document.querySelector(".cover").setAttribute("class","hidden");
