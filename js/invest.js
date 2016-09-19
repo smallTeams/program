9433a72db161f0935d75f9ebc002f1d17f2c8e48
webzhanghua <zhanghua163163@163.com>
2016-09-19 22:14:56 +0800

    底部导航修改 我要出借——详情页（invest.html）调整

--- js/invest.js
+++ js/invest.js
@@ -1,6 +1,42 @@
 jQuery(".slideGroup .slideBox").slide({ mainCell:"ul",vis:4,prevCell:".sPrev",nextCell:".sNext",effect:"leftLoop"});
 
 
+//下拉菜单
+jQuery(".invest-select span").click(function(){
+    jQuery(this).next().toggleClass("hidden");
+});
+// 关闭下拉菜单
+jQuery(".invest-select ul li").click(function(){
+    jQuery(this).parent().toggleClass("hidden");
+    jQuery(this).parents(".invest-select").children("span").html(jQuery(this).html());
+});
+
+  
+function scroll(id) {  
+jQuery("#" + id).HoverTreeScroll(1000);  
+}  
+
+
+//点击图片放大效果
+jQuery(".lists .tempWrap .pic a").click(function(){
+    // console.log(jQuery(this).find("img").attr("src"));
+    jQuery(".invest-cover").removeClass("hidden").find("img").attr("src",jQuery(this).find("img").attr("src"));
+    document.documentElement.style.overflow='hidden';
+})
+//关闭放大效果
+jQuery(".invest-cover div .close_cn").click(function(){
+    jQuery(".invest-cover").addClass("hidden");
+    document.documentElement.style.overflow='';
+})
+
+//导航跳转及选中功能
+jQuery(".lists .nav-tabs>li").click(function(){
+    jQuery(".lists .nav-tabs>li").removeClass("active");
+    jQuery(this).addClass("active");
+})
+
+
+
 //底部翻页导航
 jQuery(".financing-foot-nav ul li").click(function(){
     var $array=jQuery(".financing-foot-nav ul li");
